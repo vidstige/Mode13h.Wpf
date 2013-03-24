724744af65a1038c9679806b735855058927705a
Samuel Carlsson <samuel.carlsson@gmail.com>
2013-03-24 22:59:36 +0100

    Initial commit

---
+++ Mode13h.Wpf/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Mode13h.Wpf
+{
+    class MainViewModel
+    {
+        private byte[] _screen = new byte[320 * 200];
+        private readonly WriteableBitmap _screenBitmap;
+        private readonly BitmapPalette _palette = BitmapPalettes.Gray256;
+        private static Int32Rect _rect = new Int32Rect(0, 0, 320, 200);
+
+        public MainViewModel()
+        {
+            _screenBitmap = new WriteableBitmap(320, 200, 96, 96, PixelFormats.Indexed8, _palette);
+        }
+
+        public WriteableBitmap Screen { get { return _screenBitmap; } }
+
+        public void Draw()
+        {
+            int c=0;
+            for (int y = 0; y < 200; y++)
+            {
+                for (int x = 0; x < 200; x++)
+                {
+                    _screen[c++] = (byte)c;
+                }
+            }
+
+            _screenBitmap.WritePixels(_rect, _screen, 320, 0);
+        }
+    }
+}
---
+++ Mode13h.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Mode13h.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer _timer = new DispatcherTimer();
+        private MainViewModel _viewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _viewModel = new MainViewModel();
+            DataContext = _viewModel;
+
+            _timer.Interval = TimeSpan.FromMilliseconds(40);
+            _timer.Tick += _timer_Tick;
+        }
+
+        private void _timer_Tick(object sender, System.EventArgs e)
+        {
+            _viewModel.Draw();
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            _timer.Start();
+        }
+
+        private void Window_Unloaded_1(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+        }
+    }
+}
